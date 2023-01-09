a1f200a52e68c021e206de956959d5cfe2f00767
Mohsen <mohsenbaqeri10@gmail.com>
2023-01-09 20:18:39 +0330

    refactor: light theme

--- app/views.py
+++ app/views.py
@@ -1,29 +1,29 @@
-from django.db.models import Q
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from rest_framework import generics, status
-from rest_framework.response import Response
-
-from app.models import UserForm
+from django.shortcuts import render
+from django.views.decorators.cache import never_cache
 from app.serializers import UserFormSerializer
 
 
+@never_cache
 def index(request):
     return render(request, 'index.html', {})
 
 
+@never_cache
 def gallery(request):
     return render(request, 'gallery.html', {})
 
 
+@never_cache
 def reservation(request):
     return render(request, 'reservation.html', {})
 
 
+@never_cache
 def about(request):
     return render(request, 'about.html', {})
 
-
+@never_cache
 def contact(request):
     return render(request, 'contact.html', {})
 
